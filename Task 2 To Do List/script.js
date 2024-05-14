61f555d9c2098ca7fc91c8e8cf934d849e5155c2
minnukota381 <minnukota381@gmail.com>
2024-05-14 17:33:56 +0530

    update | QCW

--- Task 2 To Do List/script.js
+++ Task 2 To Do List/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('a[href^="#"]').on('click', function(e) {
         e.preventDefault();
-        var target = this.hash;
+        let target = this.hash;
         $('html, body').animate({
             scrollTop: $(target).offset().top
         }, 500);
